e00443cc7fbe02f1a2bc3d453c24ba785c054354
RantJames <antony.rubin.95@gmail.com>
2022-02-24 12:04:08 -0400

    Committing POST method, class based verbs and search parameters

--- demoapp/urls.py
+++ demoapp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns =   [
     path('home/', views.home, name='home'),
-    path('hotellist/', views.Hotels_List, name='hotellist')
+    path('hotellist/', views.Hotels_List, name='hotellist'),
+    path('hotellist/<str:pk>', views.hotel_detail, name='hoteldetail'),
+    path('hotel_gen_list/', views.getGenericsList.as_view(), name='hotel_gen_list')
+
 ]
--- demoapp/views.py
+++ demoapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import filters
 from .serializers import HotelSerializer
 
 from .models import Hotels
@@ -24,3 +26,19 @@ def Hotels_List(request):
         if serialize_request_data.is_valid():
             serialize_request_data.save()
         return Response({"Message": "Added Successfully"})
+
+
+@api_view(['GET', 'POST'])
+def hotel_detail(request, pk):
+    if request.method == 'GET':
+        hotels_list = Hotels.objects.get(id=pk)
+        hotelserializer = HotelSerializer(hotels_list, many=False)
+        return Response(hotelserializer.data)
+
+
+class getGenericsList(generics.ListCreateAPIView):
+    queryset = Hotels.objects.all()
+    serializer_class = HotelSerializer
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
